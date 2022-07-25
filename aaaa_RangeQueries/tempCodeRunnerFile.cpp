3a82922d27adb39b0149d6ab61bab02db5b09bb1
eklavya <eklavyakumarsingh100@gmail.com>
2022-07-25 17:39:01 +0530

    commit

--- aaaa_RangeQueries/tempCodeRunnerFile.cpp
+++ aaaa_RangeQueries/tempCodeRunnerFile.cpp
@@ -1,13 +1,29 @@
-A[i][j] = 'X';
-        // if(A[i][j-1]=='O'){
-        //     flip(i,j-1);
-        // }
-        // if(A[i-1][j]=='O'){
-        //     flip(i-1,j);
-        // }
-        // if(A[i+1][j]=='O'){
-        //     flip(i+1,j);
-        // }
-        // if(A[i][j+1]=='O'){
-        //     flip(i,j+1);
-        // }
+n = A; SCC();
+    // if(cnt==2)return 1;
+    // int indeg[cnt]{};
+    // for(int i=1;i<=n;i++){
+    //     for(auto node : graph[i]){
+    //         if(ans[i]!=ans[node])indeg[ans[node]]++;
+    //     }
+    // }
+    // int start = 1;
+    // for(int i=1;i<cnt;i++){
+    //     if(indeg[i]==0){
+    //         start = i;
+    //         break;
+    //     }
+    // }
+    // int u = 1;
+    // for(int i=1;i<=n;i++){
+    //     if(ans[i]==start){
+    //         u = i; break;
+    //     }
+    // }
+    // int done[n+1]{}; cnt = 0;
+    // function<void(int)> dfs = [&](int node){
+    //     done[node] = 1; cnt++;
+    //     for(auto i : graph[node]){
+    //         if(!done[i])dfs(i);
+    //     }
+    // };
+    // dfs(u);

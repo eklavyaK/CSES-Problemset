ffb3822a7d80286b9c02be08623f85c274771b55
eklavya <eklavyakumarsingh100@gmail.com>
2022-08-13 17:42:42 +0530

    commit

---
+++ aaaa_Mathematics/Exponentiation.cpp
@@ -0,0 +1,43 @@
+#define Compare(u) class Comp{public: bool operator() (u a, u b){return a.F < b.F;}};
+#define rapid_iostream cin.tie(0)->sync_with_stdio(0)
+#define _pq(u) priority_queue<u,vector<u>, Comp>
+#define binary(n,k) bitset<k>(n).to_string()
+#define println(n) cout<<n<<'\n'
+#define Y() cout<<"YES"<<endl
+#define print(n) cout<<n<<' '
+#define N() cout<<"NO"<<endl
+#define pii pair<int,int>
+#define mod1 1000000007ll
+#define pli pair<ll,int>
+#define pil pair<int,ll>
+#define mod2 998244353ll
+#include<bits/stdc++.h>
+#define pll pair<ll,ll>
+typedef long double ld;
+typedef long long ll;
+#define mp make_pair
+using namespace std;
+#define endl '\n'
+#define S second
+#define F first
+Compare(pii)
+/***************************************************MAIN PROGRAM*******************************************************/
+const int mod = 1e9+7;
+int exp(ll a, ll n){
+    ll ans = 1;
+    while(n>0){
+        if(n&1)ans = (ans*a)%mod;
+        a = (a*a)%mod; n>>=1;
+    }
+    return ans;
+}
+ 
+int main(){
+    rapid_iostream;
+    int n; cin>>n;
+    while(n--){
+        int a,p; cin>>a>>p;
+        cout<<exp(a,p)<<endl;
+    }
+    return 0;
+}
